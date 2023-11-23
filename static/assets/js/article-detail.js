4bdc7fffd7083521b3e05efe3ff7b65c2c96ee1d
vadrums <vadlambrianov@gmail.com>
2023-11-23 15:03:40 +0300

    Chizik page added

--- static/assets/js/article-detail.js
+++ static/assets/js/article-detail.js
@@ -70,3 +70,32 @@ function makeSticky() {
 		},
 		speed: 800,
 	})
+
+  /* FAQ ACC */
+  var acc = document.getElementsByClassName("blog-article-content-faq__accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+
+/* CONTENTS MOBILE ACC */
+var accContents = document.getElementsByClassName("blog-article-contents-mobile__container-top");
+console.log(accContents);
+accContents[0].addEventListener("click", function() {
+  this.classList.toggle("active"); 
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  } 
+});
